9fca73b485c6734c44b750bba236766a1ab75270
Jack De Winter <wyld_sider@yahoo.com>
2023-09-23 22:05:41 -0700

    https://github.com/jacobwinter314/art-of-the-possible/issues/14 - added k8s to TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,8 @@ locals {
   environment_name = "dev"
   location         = "westus"
 
+  cluster_username = "clusteradmin"
+
   common_tags = {
     Component = "ArtOfThePossible"
   }
@@ -34,7 +36,62 @@ module "container-registry" {
   sku_name      = var.acr_sku_name
 }
 
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "aks-${local.workload_name}-${local.environment_name}-${local.location}"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
 
+  dns_prefix          = "dns-aks-${local.workload_name}-${local.environment_name}-${local.location}"
+
+  identity {
+    type = "UserAssigned"
+    user_assigned_identity_id  = azurerm_user_assigned_identity.k8s_identity.id
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = 1
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  linux_profile {
+    admin_username = local.cluster_username
+
+    ssh_key {
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_user_assigned_identity" "k8s_identity" {
+  name                = "id-${local.workload_name}-${local.environment_name}-${local.location}"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_role_assignment" "acr_role" {
+  scope                = module.container-registry.acr_azure_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.k8s_identity.principal_id
+  skip_service_principal_aad_check = true
+}
+  
 output "acr_server_url" {
   description = "Url used to log in to the ACR with."
   value       = module.container-registry.acr_server_url
@@ -49,4 +106,8 @@ output "acr_client_secret" {
   description = "If admin_enabled, the admin password for the ACR."
   value       = module.container-registry.admin_password
   sensitive   = true
+}
+
+output "key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
 }
---
+++ terraform/ssh.tf
@@ -0,0 +1,17 @@
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  resource_id = azapi_resource.ssh_public_key.id
+
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  action      = "generateKeyPair"
+  method      = "POST"
+
+  response_export_values = ["publicKey", "privateKey"]
+}
+
+resource "azapi_resource" "ssh_public_key" {
+  name      = "ssh-${local.workload_name}-${local.environment_name}-${local.location}"
+  location  = azurerm_resource_group.main_rg.location
+
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  parent_id = azurerm_resource_group.main_rg.id
+}
